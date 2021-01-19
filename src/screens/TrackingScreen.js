9244f9e5adc9f8afa8e840f1e829d278a883c8e1
shashank1501 <shashank.maurya@mapmyindia.com>
2021-01-19 10:30:45 +0530

    config added

--- src/screens/TrackingScreen.js
+++ src/screens/TrackingScreen.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet, Text, View, TouchableOpacity, Linking} from 'react-native';
-import MapmyIndiaIntouch from "mapmyindia-intouch-react-native-sdk";
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+import MapmyIndiaIntouch from 'mapmyindia-intouch-react-native-sdk';
 import play from '../assets/play_arrow_24_px.png';
 import stop from '../assets/stop_24_px.png';
 import Toast from 'react-native-simple-toast';
+import {Picker} from '@react-native-picker/picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class TrackingScreen extends Component {
   constructor(props) {
@@ -11,14 +20,18 @@ class TrackingScreen extends Component {
     this.state = {
       icon: play,
       text: 'Start Tracking',
+      currentSpeed: MapmyIndiaIntouch.BEACON_PRIORITY_FAST,
+      dropDownEnabled: true,
+      pickerBackground: '#CCFF0000',
     };
   }
 
   async componentDidMount() {
     MapmyIndiaIntouch.addTrackingStateListener((event) => {
-     Toast.show(event);
+      Toast.show(event);
       console.log(event);
     });
+    this.getData();
     const status = await MapmyIndiaIntouch.isRunning();
     this.setIconsAndLabel(status);
   }
@@ -27,33 +40,56 @@ class TrackingScreen extends Component {
     MapmyIndiaIntouch.removeTrackingStateListener();
   }
 
-  trackButtonPress = async () => {
-    const status = await MapmyIndiaIntouch.isRunning()
-   
-    if (status) {
-        this.setIconsAndLabel(false);
-        MapmyIndiaIntouch.stopTracking();
-      } else {
-        this.setIconsAndLabel(true);
-        MapmyIndiaIntouch.startTracking();
-      }
-   
+  storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem('@storage_Key', JSON.stringify(value));
+    } catch (e) {
+      // saving error
+    }
   };
 
-  redirectButtonPress=()=>{
-      Linking.openURL("https://intouch.mapmyindia.com")
-  }
+  getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@storage_Key');
+      if (value !== null) {
+        this.setState({
+          currentSpeed: parseInt(value),
+        });
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+  trackButtonPress = async () => {
+    const status = await MapmyIndiaIntouch.isRunning();
+    // console.log(status);
+    if (status) {
+      this.setIconsAndLabel(false);
+      MapmyIndiaIntouch.stopTracking();
+    } else {
+      this.setIconsAndLabel(true);
+      MapmyIndiaIntouch.startTracking(this.state.currentSpeed);
+    }
+  };
+
+  redirectButtonPress = () => {
+    Linking.openURL('https://intouch.mapmyindia.com');
+  };
 
   setIconsAndLabel = (status) => {
     if (status) {
       this.setState({
         icon: stop,
         text: 'Stop tracking',
+        dropDownEnabled: false,
+        pickerBackground: 'rgba(52, 52, 52, 0.3)',
       });
     } else {
       this.setState({
         icon: play,
         text: 'Start tracking',
+        dropDownEnabled: true,
+        pickerBackground: 'rgba(255, 140, 0, 0.3)',
       });
     }
   };
@@ -66,7 +102,36 @@ class TrackingScreen extends Component {
           style={styles.logo}
         />
         <Text style={styles.label}>Live Location Tracking</Text>
-        <View style={{flex: 2, justifyContent: 'center'}}>
+        <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{fontWeight:'bold',color:'grey'}}>Choose Speed</Text>
+          <View
+            style={{
+              ...styles.picker,
+              backgroundColor: this.state.pickerBackground,
+              borderColor: this.state.pickerBackground,
+            }}>
+            <Picker
+              enabled={this.state.dropDownEnabled}
+              selectedValue={this.state.currentSpeed}
+              style={{height: 50, width: 140}}
+              onValueChange={(itemValue, itemIndex) => {
+                this.storeData(itemValue);
+                this.setState({currentSpeed: itemValue});
+              }}>
+              <Picker.Item
+                label="FAST"
+                value={MapmyIndiaIntouch.BEACON_PRIORITY_FAST}
+              />
+              <Picker.Item
+                label="SLOW"
+                value={MapmyIndiaIntouch.BEACON_PRIORITY_SLOW}
+              />
+              <Picker.Item
+                label="OPTIMAL"
+                value={MapmyIndiaIntouch.BEACON_PRIORITY_OPTIMAL}
+              />
+            </Picker>
+          </View>
           <TouchableOpacity onPress={this.trackButtonPress}>
             <View style={styles.trackButton}>
               <Image source={this.state.icon} style={{height: 30, width: 30}} />
@@ -74,13 +139,17 @@ class TrackingScreen extends Component {
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={this.redirectButtonPress} style={{marginTop:10}}>
-            <View style={{flexDirection:'row',marginTop:40}}>
+          <TouchableOpacity
+            onPress={this.redirectButtonPress}
+            style={{marginTop: 10}}>
+            <View style={{flexDirection: 'row', marginTop: 40}}>
               <Image
                 source={require('../assets/open_in_new_24_px.png')}
                 style={{height: 20, width: 20}}
               />
-              <Text style={{fontWeight: 'bold',color:'#33B3FF'}}>Redirect to InTouch</Text>
+              <Text style={{fontWeight: 'bold', color: '#33B3FF'}}>
+                Redirect to InTouch
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -112,8 +181,13 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5,
     paddingRight: 10,
-    width:130,
+    width: 130,
     borderRadius: 10,
+  },
+  picker: {
+    marginBottom: 20,
+    borderWidth: 1,
+    borderRadius: 5,
   },
 });
 
